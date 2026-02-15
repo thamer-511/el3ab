cdb49a887cd2bdb7a09fe6e1fec50f2520a1950c
thamer-511 <kfuthamer511@gmail.com>
2026-02-15 14:59:18 +0300

    Delete functions directory

--- functions/_worker.ts
+++
@@ -1,9 +0,0 @@
-import { HurufSessionDO } from './do/HurufSessionDO';
-
-export { HurufSessionDO };
-
-export default {
-  async fetch(request: Request): Promise<Response> {
-    return new Response(`No route matched: ${new URL(request.url).pathname}`, { status: 404 });
-  },
-};
